d972587f44e63cc81a14fa067bb067a416399408
Pedro Alexandre <pedroalexandre.bastos@hotmail.com>
2025-10-03 18:34:28 -0300

    add remove first node method

--- src/main/java/com/example/dsa/DsaApplication.java
+++ src/main/java/com/example/dsa/DsaApplication.java
@@ -6,10 +6,24 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DsaApplication {
 
     public static void main(String[] args) {
+        // prepend();
+        removeFirst();
+    }
 
+    private static void removeFirst() {
+        LinkedList myLinkedList = new LinkedList(2);
+        myLinkedList.append(3);
+        System.out.println("Linked list with 2 items, node removed is of value: " + myLinkedList.removeFirst().value);
+        System.out.println("Linked list with 1 item, node removed is of value: " + myLinkedList.removeFirst().value);
+        System.out.println("Linked list with 0 items, this list is " + myLinkedList.removeFirst());
+    }
+
+    private static void prepend() {
         LinkedList myLinkedList = new LinkedList(2);
         myLinkedList.append(3);
         myLinkedList.prepend(1);
         myLinkedList.printList();
     }
+
+
 }
--- src/main/java/com/example/dsa/LinkedList.java
+++ src/main/java/com/example/dsa/LinkedList.java
@@ -69,6 +69,18 @@ public class LinkedList {
     }
 
 
+    public Node removeFirst() {
+        if (length == 0) return null;
+        Node temp = head;
+        head = head.next;
+        temp.next = null;
+        length--;
+        if (length == 0) {
+            tail = null;
+        }
+        return temp;
+    }
+
     public void getHead() {
         System.out.println("Head: " + head.value);
     }
