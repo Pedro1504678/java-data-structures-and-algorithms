af45e2a5938ea28ef600ea81d0ac892c191c3f12
Pedro Alexandre <pedroalexandre.bastos@hotmail.com>
2025-10-03 17:31:31 -0300

    testing prepend method with print linked list in console

--- src/main/java/com/example/dsa/DsaApplication.java
+++ src/main/java/com/example/dsa/DsaApplication.java
@@ -7,17 +7,9 @@ public class DsaApplication {
 
     public static void main(String[] args) {
 
-        LinkedList myLinkedList = new LinkedList(1);
-        myLinkedList.append(2);
-
-        // (2) Items - Returns removed node value 2
-        System.out.println(myLinkedList.removeLast().value);
-
-        // (1) Items - Returns removed node value 1
-        System.out.println(myLinkedList.removeLast().value);
-
-        // (0) Items - Returns null
-        System.out.println(myLinkedList.removeLast());
-
+        LinkedList myLinkedList = new LinkedList(2);
+        myLinkedList.append(3);
+        myLinkedList.prepend(1);
+        myLinkedList.printList();
     }
 }
